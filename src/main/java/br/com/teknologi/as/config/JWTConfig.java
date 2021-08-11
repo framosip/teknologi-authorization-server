a9a2d94540aed01ca72dc18b1cde506d5432cf80
Fernando Ramos <framosip@gmail.com>
2021-08-10 23:18:42 -0300

    JWT configuration beans

---
+++ src/main/java/br/com/teknologi/as/config/JWTConfig.java
@@ -0,0 +1,33 @@
+package br.com.teknologi.as.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@Slf4j
+public class JWTConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        log.info("[Configuration] ===== BCryptPasswordEncoder configured =====");
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        log.info("[Configuration] ===== JwtAccessTokenConverter configured =====");
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        log.info("[Configuration] ===== JwtTokenStore configured =====");
+        return new JwtTokenStore(this.accessTokenConverter());
+    }
+}
