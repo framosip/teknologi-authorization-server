af349edd23b921c03407da55cbf096d5be4aa878
Fernando Ramos <framosip@gmail.com>
2021-08-10 21:24:35 -0300

    Spring boot project created

---
+++ src/test/java/br/com/teknologi/as/AuthorizationServerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teknologi.as;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
